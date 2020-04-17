2cff01f20576ab2ebb2055bf77c9918522adcf05
Rabin <gautamrabinraj@gmail.com>
2020-04-17 23:32:25 +0545

    next one

--- god/urls.py
+++ god/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from pages.views import welcome
+from pages.views import welcome, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', welcome)
+    path('', welcome),
+    path('about/', about),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from datetime import datetime
+from meetings.models import Meeting
 # Create your views here.
 
 def welcome(request):
-    return HttpResponse("Welcome to god.how are you")
+    return render(request, "pages/welcome.html", 
+        {"number_meet": Meeting.objects.count()})
+def about(request):
+    return HttpResponse(str(datetime.now()))
